375d93a8422a3c75590fb27fbc553cd8e3169a2b
Sean Mayo <jdhbnoj7s@mozmail.com>
2023-01-09 14:04:38 -0800

    let's try this again

--- setup.bash
+++ setup.bash
@@ -194,8 +194,13 @@ menu() {
 			tar -czf manpages.tar.gz **/*.gz
 			tar -tvf manpages.tar.gz
 			local release_description=$(git log -1 --pretty=%B | cat)
-			local release_response=$(curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $2" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/shatteredsword/gitup/releases -d '{"tag_name":$gitup_version,"target_commitish":"main","name":$gitup_version,"body":$release_description,"draft":false,"prerelease":false,"generate_release_notes":false}')
-			echo $release_response
+			curl -X POST \
+			-H "Accept: application/vnd.github+json" \
+			-H "Authorization: Bearer $2" \
+			-H "X-GitHub-Api-Version: 2022-11-28" \
+			https://api.github.com/repos/shatteredsword/gitup/releases \
+			-d "{'tag_name':${gitup_version},'target_commitish':'main','name':${gitup_version},'body':${release_description},'draft':false,'prerelease':false,'generate_release_notes':false}"
+			
 			# release_id=$("$release_response" | jq '.id' )
 			# echo "release_id: $release_id"
 			# local asset_response=$(curl -X POST --data-binary @manpages.tar.gz -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $2" -H "X-GitHub-Api-Version: 2022-11-28" -H "Content-Type: application/x-gtar" https://uploads.github.com/repos/shatteredsword/gitup/releases/$release_id/assets?name=manpages.tar.gz)
