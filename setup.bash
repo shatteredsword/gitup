ee0a4cfbce3af7173899e9d7e9bd4e9eae2eacce
Sean Mayo <jdhbnoj7s@mozmail.com>
2023-01-09 11:50:57 -0800

    still no

--- setup.bash
+++ setup.bash
@@ -179,7 +179,7 @@ menu() {
 		tar -tvf manpages.tar.gz
 		curl \
 		-H "Accept: application/vnd.github+json" \
-		-H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}"\
+		-H "Authorization: Bearer $2"\
 		-H "X-GitHub-Api-Version: 2022-11-28" \
 		https://api.github.com/repos/shatteredsword/gitup/releases/v0.0.1/assets
 	else
