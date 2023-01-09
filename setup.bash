3eb59d0f317443d2c2ee16be98d0b9082a3a3878
Sean Mayo <jdhbnoj7s@mozmail.com>
2023-01-09 14:44:06 -0800

    removed asset upload to test local

--- setup.bash
+++ setup.bash
@@ -203,15 +203,16 @@ menu() {
 			-d '{"tag_name":"'"$gitup_version"'","target_commitish":"main","name":"'"$gitup_version"'","body":"'"$release_description"'","draft":false,"prerelease":false,"generate_release_notes":false}'
 			)
 			release_id=$(echo "$release_response" | jq '.id')
-			local asset_response=$(
-			curl -X POST \
-			--data-binary @manpages.tar.gz \
-			-H "Accept: application/vnd.github+json" 
-			-H "Authorization: Bearer $2" \
-			-H "X-GitHub-Api-Version: 2022-11-28" \
-			-H "Content-Type: application/x-gtar" \
-			"https://uploads.github.com/repos/shatteredsword/gitup/releases/$release_id/assets?name=manpages.tar.gz"
-			)
+			echo "release_id: $release_id"
+			# local asset_response=$(
+			# curl -X POST \
+			# --data-binary @manpages.tar.gz \
+			# -H "Accept: application/vnd.github+json" 
+			# -H "Authorization: Bearer $2" \
+			# -H "X-GitHub-Api-Version: 2022-11-28" \
+			# -H "Content-Type: application/x-gtar" \
+			# "https://uploads.github.com/repos/shatteredsword/gitup/releases/$release_id/assets?name=manpages.tar.gz"
+			# )
 		fi
 	else
 		local_install
